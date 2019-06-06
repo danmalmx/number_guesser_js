199401cfd94518bff649a974315ca500f168fc21
danmalmx <danmalmx@gmail.com>
2019-06-06 20:50:30 +0200

    add event listener and function (inkl message) for correct guess

--- app.js
+++ app.js
@@ -11,3 +11,33 @@ const game = document.querySelector("#game"),
   guessBtn = document.querySelector("#guess-btn"),
   guessInput = document.querySelector("#guess-input"),
   message = document.querySelector(".message");
+
+//Assign min and max values in UI
+minNumb.textContent = min;
+maxNumb.textContent = max;
+
+//Listen for guess
+guessBtn.addEventListener("click", function() {
+  let guess = parseInt(guessInput.value);
+
+  //Validate
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, "red");
+  }
+
+  //Check if winning number
+  if (guess === winningNum) {
+    //Dissable input
+    guessInput.disabled = true;
+    //Border green by win
+    guessInput.style.borderColor = "green";
+    //Message that user won
+    setMessage(`${winningNum} is correct! You win!`, "green");
+  } else {
+  }
+});
+
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
