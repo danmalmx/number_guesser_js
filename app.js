827bbbea60bc4ecf1432b25dfbd85e3451286359
danmalmx <danmalmx@gmail.com>
2019-06-07 09:11:11 +0200

    adding function to randomize winning number

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Game values
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandomNum(min, max),
   guessesLeft = 3;
 
 //UI elements
@@ -73,6 +73,11 @@ function gameOver(won, msg) {
   //Play again
   guessBtn.value = "Play Again";
   guessBtn.className += "play-again";
+}
+
+//Get winn ing number randomly
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 //Set message
